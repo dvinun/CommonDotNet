8198dfa0c79a6e9176a4a351656e3aa1409959f6
vinayaka amaresh <dvinun@outlook.com>
2019-06-04 01:10:25 -0700

    added project files

---
+++ CommonDotNet/UnitTestFrameworkUtils.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Dvinun.Common.Utils
+{
+    public class UnitTestFrameworkUtils
+    {
+        public static void CompareArrays<T>(T[] expected, T[] actual)
+        {
+            Assert.AreEqual(expected == null, actual == null, "Expected {0}null value and {1}null found.", expected == null ? "" : "not", actual == null ? "" : "not");
+            if (expected == null || actual == null)
+                return;
+
+            Assert.AreEqual(expected.LongLength, actual.LongLength, "Expected Length is {0} actual: {1}", expected.LongLength, actual.LongLength);
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i], "Values on index {0} are not equal. Expected {1} actual: {2}", i, expected[i], actual[i]);
+            }
+        }
+    }
+}
