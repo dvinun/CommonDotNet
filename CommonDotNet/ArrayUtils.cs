6b8919e80bbf1d0f19767b401bcbb224b4044248
vinayaka amaresh <dvinun@outlook.com>
2019-07-07 19:52:27 -0700

    new files

---
+++ CommonDotNet/ArrayUtils.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dvinun.CommonDotNet.Utils
+{
+    public class Array
+    {
+        public static List<List<int>> Get2DArrayFromString(string arrayInput, int m, int n)
+        {
+            List<List<int>> matrix = new List<List<int>>();
+
+            for (int i = 0; i < m; i++)
+            {
+                matrix.Add(arrayInput.Split(' ').ToList().
+                    Select(matrixTemp => Convert.ToInt32(matrixTemp)).ToList());
+            }
+
+            return matrix;
+        }
+    }
+}
--- CommonDotNet/UnitTestFrameworkUtils.cs
+++ CommonDotNet/UnitTestFrameworkUtils.cs
@@ -18,5 +18,27 @@ namespace Dvinun.CommonDotNet.Utils
                 Assert.AreEqual(expected[i], actual[i], "Values on index {0} are not equal. Expected {1} actual: {2}", i, expected[i], actual[i]);
             }
         }
+
+        public static void Compare2DArrays<T>(T[,] array1, T[,] array2)
+        {
+            foreach (T array1Item in array1)
+            {
+                foreach (T array2Item in array2)
+                {
+                }
+            }
+
+
+            //Assert.AreEqual(expected == null, actual == null, "Expected {0}null value and {1}null found.", expected == null ? "" : "not", actual == null ? "" : "not");
+            //if (expected == null || actual == null)
+            //    return;
+
+            //Assert.AreEqual(expected.LongLength, actual.LongLength, "Expected Length is {0} actual: {1}", expected.LongLength, actual.LongLength);
+
+            //for (int i = 0; i < expected.Length; i++)
+            //{
+            //    Assert.AreEqual(expected[i], actual[i], "Values on index {0} are not equal. Expected {1} actual: {2}", i, expected[i], actual[i]);
+            //}
+        }
     }
 }
